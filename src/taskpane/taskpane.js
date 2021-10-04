08f2f9f70027815908cd0fad141b7458a4d4cbc2
steffens1 <wildergq58.wg@gmail.com>
2021-10-04 14:46:31 -0500

    git final

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -3,20 +3,9 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import _ from 'lodash'
-import db from './database.js'
 import "./assets/index.css"
 
-async function test(){
-  try {
-    await db.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-  
-}
 
-test()
 
 Vue.use(ElementUI)
 window._ = _
