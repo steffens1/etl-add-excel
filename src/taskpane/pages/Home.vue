ffb9247a1badb0527c0a595228fa97a089242ffb
steffens1 <wildergq58.wg@gmail.com>
2021-11-16 11:19:18 -0500

    Co-authored-by: Luis Montesinos <Gorgontes@users.noreply.github.com>

--- src/taskpane/pages/Home.vue
+++ src/taskpane/pages/Home.vue
@@ -1,69 +1,48 @@
 <template>
-  <el-table
-    :data="data"
-    stripe
-    style="width: 100%">
-    <el-table-column
-      prop="NOMBRE COMERCIAL"
-      label="NOMBRE COMERCIAL">
-    </el-table-column>
-    
-    <el-table-column
-      prop="PLAN"
-      label="PLAN">
-    </el-table-column>
-    
-    <el-table-column
-      prop="Valor"
-      label="Valor">
-    </el-table-column>
-    
-    <el-table-column
-      prop="MODALIDAD"
-      label="MODALIDAD">
-    </el-table-column>
-    
-    <el-table-column
-      prop="CAMPANA"
-      label="CAMPANA">
-    </el-table-column>
-
-  </el-table>
+  <div>
+    <el-button @click="enviardata">enviar datta</el-button>
+    <el-input v-model="slice"></el-input>
+  </div>
 </template>
 
 
 <script>
+
   export default {
     data() {
       return {
         SheetList: [],
         SheetCategorized:[],
-        data: {}
+        data: {},
+        slice :  100
       }
     },
+
     created(){
       Excel.run(async (context) => {
         var sheets = context.workbook.worksheets;
         var arraySheets = sheets.load("items/name");
-
-        var sheetPrecio = context.workbook.worksheets.getItem("CUOTAS RENO 12M");
+        var sheetPrecio = context.workbook.worksheets.getItem("L  Exclusiva C Porta  12M");
         var tablaRange = sheetPrecio.getUsedRange().load("values")
-
-        
         await context.sync() 
-
         var headers = _.compact(tablaRange.values[0])
         var values = []
-
         tablaRange.values.map((item)=>{
          values.push(_.zipObject(headers, item ))
         })
-
-        this.data = values
-        console.log(values)
-      
+        this.data = values     
       })
+    },
+    methods : {
+      enviardata : function (){
+        var sliceSize = parseInt(this.slice)
+        const resul = this.data.slice(1,sliceSize)
+        this.$http.post('/update-precios-productos', {
+          data : resul
+        })
+      }
     }
   };
+  
 </script>
 
